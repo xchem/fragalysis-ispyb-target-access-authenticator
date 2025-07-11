18d1c791dc0a5ad3b4932f6490e68a17f68a1a66
Alan Christie <alan.christie@matildapeak.com>
2025-07-11 10:39:59 +0100

    feat: Initial logic

--- README.md
+++ README.md
@@ -55,6 +55,35 @@ that are not known: -
 }
 ```
 
+## Contributing
+The project uses: -
+
+- [pre-commit] to enforce linting of files prior to committing them to the
+  upstream repository
+- [Commitizen] to enforce a [Conventional Commit] commit message format
+- [Black] as a code formatter
+- [Poetry] as a package manager (for the b/e)
+
+You **MUST** comply with these choices in order to  contribute to the project.
+
+To get started review the pre-commit utility and the conventional commit style
+and then set-up your local clone by following the **Installation** and
+**Quick Start** sections: -
+
+    poetry shell
+    poetry install --with dev
+    pre-commit install -t commit-msg -t pre-commit
+
+Now the project's rules will run on every commit, and you can check the
+current health of your clone with: -
+
+    pre-commit run --all-files
+
 ---
 
+[black]: https://black.readthedocs.io/en/stable
+[commitizen]: https://commitizen-tools.github.io/commitizen/
+[conventional commit]: https://www.conventionalcommits.org/en/v1.0.0/
 [fastapi]: https://fastapi.tiangolo.com
+[poetry]: https://python-poetry.org
+[pre-commit]: https://pre-commit.com
---
+++ app/app.py
@@ -0,0 +1,97 @@
+"""The entrypoint for the Fragalysis Stack FastAPI ISPyB Target Access Authenticator."""
+
+import json
+import logging
+import os
+from logging.config import dictConfig
+from typing import Any
+
+from fastapi import (
+    FastAPI,
+    status,
+)
+from pydantic import BaseModel
+from pymemcache.client.base import Client, KeepaliveOpts
+from pymemcache.client.retrying import RetryingClient
+from pymemcache.exceptions import MemcacheUnexpectedCloseError
+
+# Configure logging
+print("Configuring logging...")
+_LOGGING_CONFIG: dict[str, Any] = {}
+with open("logging.config", "r", encoding="utf8") as stream:
+    try:
+        _LOGGING_CONFIG = json.loads(stream.read())
+    except json.decoder.JSONDecodeError as exc:
+        print(exc)
+dictConfig(_LOGGING_CONFIG)
+print("Configured logging.")
+
+_LOGGER = logging.getLogger(__name__)
+
+app = FastAPI()
+
+# Configure memcached.
+# The location is either a host ("localhost") or host and port ("localhost:1234").
+# If the port is not the expected default of 11211 is assumed.
+_MEMCACHED_LOCATION: str = os.getenv("TAA_MEMCACHED_LOCATION", "localhost")
+_MEMCACHED_KEEPALIVE: KeepaliveOpts = KeepaliveOpts(idle=35, intvl=8, cnt=5)
+_MEMCACHED_BASE_CLIENT: Client = Client(
+    _MEMCACHED_LOCATION,
+    connect_timeout=4,
+    encoding="utf-8",
+    timeout=0.5,
+    socket_keepalive=_MEMCACHED_KEEPALIVE,
+)
+_MEMCACHED_CLIENT: RetryingClient = RetryingClient(
+    _MEMCACHED_BASE_CLIENT,
+    attempts=3,
+    retry_delay=0.01,
+    retry_for=[MemcacheUnexpectedCloseError],
+)
+
+with open("VERSION", "r", encoding="utf-8") as version_file:
+    _VERSION: str = version_file.read().strip()
+
+
+class TargetAccessGetVersionResponse(BaseModel):
+    """/version/ GET response."""
+
+    # The Kind of Authenticator (i.e. "ISPYB")
+    kind: str
+    # Our name (ours is 'Python FastAPI')
+    name: str
+    # Our version number
+    version: str
+
+
+class TargetAccessGetUserTasResponse(BaseModel):
+    """/target-access/{username}/ GET response."""
+
+    # Number of Target Access Strings in the response
+    count: int
+    # Possibly empty list of Target Access strings
+    target_access: list[str]
+
+
+# Endpoints for the 'public-facing' event-stream web-socket API ------------------------
+
+
+@app.get("/version/", status_code=status.HTTP_200_OK)
+def get_taa_version() -> TargetAccessGetVersionResponse:
+    """Returns our version information"""
+    return TargetAccessGetVersionResponse(
+        kind="ISPYB",
+        name="Python FastAPI",
+        version=_VERSION,
+    )
+
+
+@app.get("/target-access/{username}", status_code=status.HTTP_200_OK)
+def get_taa_user_tas(username: str):
+    """Returns the list of target access strings for a user."""
+    _LOGGER.info("Request for '%s'", username)
+    _ = _MEMCACHED_CLIENT.get(username)
+    return TargetAccessGetUserTasResponse(
+        count=0,
+        target_access=[],
+    )
