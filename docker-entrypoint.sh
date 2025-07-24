c3f58589e31ee94bee4940ac1c05ab32d33f06ec
Alan Christie <alan.christie@matildapeak.com>
2025-07-24 15:56:00 +0100

    fix: Fix concurrency

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,8 @@
 
 # Run the container using port 8080
 echo "+> Launching uvicorn..."
+echo "+> WORKERS=${WORKERS}"
+echo "+> CONCURRENCY=${CONCURRENCY}"
 uvicorn app.app:app --host 0.0.0.0 --port 8080 \
     --workers ${WORKERS} \
-    --concurrency ${CONCURRENCY}
+    --limit-concurrency ${CONCURRENCY}
