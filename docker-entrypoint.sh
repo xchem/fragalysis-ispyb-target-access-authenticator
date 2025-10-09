1be49bc3cd6eb72b178b6db868a9b4b196b7805c
Alan Christie <alan.christie@matildapeak.com>
2025-10-09 14:08:04 +0200

    fix: Now runs two uvicorn servers

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,10 @@
 
 #set -e
 
-# Run the container using port 8080
+# Run the container using both the customer-facing stats service
+# and the internal authentication service endpoint.
+# Done by launching two uvicorn instances in parallel.
 echo "+> Launching uvicorn..."
 echo "+> WORKERS=${WORKERS}"
-uvicorn app.app:auth --host 0.0.0.0 --port 8080 --workers ${WORKERS}
+uvicorn app.app:stats --host 0.0.0.0 --port 8081 & \
+    uvicorn app.app:auth --host 0.0.0.0 --port 8080 --workers ${WORKERS}
